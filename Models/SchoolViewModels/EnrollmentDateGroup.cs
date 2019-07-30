33dc9b37926e20338ec78f52cf986901703f9dc9
edaliuw <36862429+edaliuw@users.noreply.github.com>
2019-07-30 13:59:43 -0700

    Update EnrollmentDateGroup.cs

--- Models/SchoolViewModels/EnrollmentDateGroup.cs
+++ Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -1,6 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-
+/* Ed Ali - Replaced code with code that has a nullable datetime regarding the enrollment date and also has a count for students */
 namespace ContosoUniversity.Models.SchoolViewModels
 {
     public class EnrollmentDateGroup
